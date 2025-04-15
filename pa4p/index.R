1129656d12ceeeffc2012036457aa593a125365a
andreavcarbone <andrea.carbone@rutgers.edu>
2025-04-14 21:13:51 -0400

    up

---
+++ pa4p/index.R
@@ -0,0 +1,8 @@
+# Check if xaringan is installed; install it if not
+if (!requireNamespace("xaringan", quietly = TRUE)) {
+  install.packages("xaringan")
+}
+
+# Load the xaringan package
+library(xaringan)
+
