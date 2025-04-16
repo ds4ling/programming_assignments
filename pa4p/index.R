1f620d4db0186a637afd0ba9bb5cb9a1ac8ed243
andreavcarbone <andrea.carbone@rutgers.edu>
2025-04-15 22:45:56 -0400

    Delete pa4p directory

--- pa4p/index.R
+++
@@ -1,8 +0,0 @@
-# Check if xaringan is installed; install it if not
-if (!requireNamespace("xaringan", quietly = TRUE)) {
-  install.packages("xaringan")
-}
-
-# Load the xaringan package
-library(xaringan)
-
